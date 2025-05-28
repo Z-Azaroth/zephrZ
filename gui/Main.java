abbeb2ed9333987bbc2a5d786ad1a0ea7c2414c6
Z-Azaroth <kyrianm999@gmail.com>
2025-05-27 21:35:19 -0500

    Initial Commit

---
+++ gui/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Main {
+  public static void main(String[] args) {
+    String version = "1.0.0";
+
+    JFrame mframe = new JFrame();
+    mframe.setTitle("Zephr " + version);
+    mframe.setSize(420, 420);
+    mframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    mframe.setResizable(true);
+    mframe.setVisible(true);
+  }
+}
