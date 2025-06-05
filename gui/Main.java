97e5316fed384520a0baa243f121232164724f7a
Z-Azaroth <kyrianm999@gmail.com>
2025-06-04 23:35:28 -0500

    Made Changes to the main file
    formatted the file

--- gui/Main.java
+++ gui/Main.java
@@ -9,12 +9,9 @@ public class Main {
     mframe.setTitle("Zephr " + version);
     mframe.setSize(420, 420);
     mframe.getContentPane().setBackground(Color.black);
-
-
-
-    
     mframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     mframe.setResizable(true);
     mframe.setVisible(true);
+
   }
 }
